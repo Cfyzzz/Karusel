0d7fc2feea30706f5445c768965610f85b23d7c1
Alexander Nedovizin <parkit74@gmail.com>
2022-03-15 12:32:37 +0500

    feat (components): multiselect types

--- services/list_packages_post.py
+++ services/list_packages_post.py
@@ -1,6 +1,6 @@
-from peewee import DoesNotExist
+from peewee import DoesNotExist, DatabaseError
 
-from model import Package, Component, Type
+from model import Package, Component, Type, database
 from tools import is_valid_input_str
 from .iservise import *
 
@@ -41,21 +41,28 @@ class ListPackagesPostService(IService):
             return redirect(request.url)
         elif action == "append":
             package_name = self.request.form.get('typeName')
-            type_name = self.request.form.get('typeType')
-            the_type = Type.get_or_none(type=type_name)
+            type_names = self.request.form.getlist('typeType')
 
-            if the_type is None:
-                flash("Ошибка. Тип не распознан")
-                return redirect(request.url)
+            with database.atomic() as transaction:
+                try:
+                    for type_name in type_names:
+                        the_type = Type.get_or_none(type=type_name)
 
-            if not is_valid_input_str(package_name, 16):
-                flash("Вы ввели некорректное имя корпуса")
-                return redirect(request.url)
+                        if not the_type:
+                            flash("Ошибка. Тип не распознан")
+                            return redirect(request.url)
 
-            new_package = Package.get_or_none(package=package_name, type=the_type)
-            if new_package:
-                flash("Такой корпус уже существует. Введите другое название корпуса")
-                return redirect(request.url)
-            new_package = Package.create(package=package_name, type=the_type)
-            new_package.save()
+                        if not is_valid_input_str(package_name, 16):
+                            flash(f"Вы ввели некорректное имя корпуса: '{package_name}'")
+                            return redirect(request.url)
+
+                        new_package = Package.get_or_none(package=package_name, type=the_type)
+                        if new_package:
+                            continue
+                        new_package = Package.create(package=package_name, type=the_type)
+                        new_package.save()
+                    transaction.commit()
+                except DatabaseError:
+                    transaction.rollback()
+
         return redirect(request.url)
