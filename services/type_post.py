076b362756f98ba06f9d7ceaad2b1bd2eda8186d
Alexander Nedovizin <parkit74@gmail.com>
2022-03-22 14:07:45 +0500

    ref (packages): При добавлении количества компонентов склада- отправка карусели на адрес

--- services/type_post.py
+++ services/type_post.py
@@ -20,22 +20,7 @@ class TypePostService(IService):
             url_pop = tools.get_base_url() + "/components/pop"
             payload_pop = {'id': self.request.form.get('componentId'), 'quantity': self.request.form.get('numDec')}
             requests.put(url_pop, headers=headers, data=json.dumps(payload_pop, indent=4))
-
-            is_karusel = component.box.strip().lower()[0:1] in ["k", "к"]
-            if is_karusel:
-                address = component.address.split("-")
-                if len(address) == 2:
-                    row_device = address[0].strip()
-                    column_device = address[1].strip()
-                    floor_device = component.box[1:].strip()
-                    url = tools.get_url_karusel() \
-                          + "/get?row=" + row_device \
-                          + "&column=" + column_device \
-                          + "&floor=" + floor_device
-                    try:
-                        requests.get(url, timeout=3)
-                    except requests.exceptions.ConnectTimeout:
-                        flash("На карусель запрос не ушёл :(")
+            self.open_karusel(component)
         elif self.request.form.get('componentOp') == "inc":
             url = tools.get_base_url() + "/components/push"
             payload_push = {'id': component.id,
@@ -45,8 +30,27 @@ class TypePostService(IService):
                             'type': component.type.type
                             }
             requests.put(url, headers=headers, data=json.dumps(payload_push, indent=4))
+            self.open_karusel(component)
         elif self.request.form.get('componentOp') == "delete":
             url_pop = tools.get_base_url() + "/components/pop"
             payload_pop = {'id': self.request.form.get('componentId')}
             requests.delete(url_pop, headers=headers, data=json.dumps(payload_pop, indent=4))
         return redirect(request.url)
+
+    @staticmethod
+    def open_karusel(component):
+        is_karusel = component.box.strip().lower()[0:1] in ["k", "к"]
+        if is_karusel:
+            address = component.address.split("-")
+            if len(address) == 2:
+                row_device = address[0].strip()
+                column_device = address[1].strip()
+                floor_device = component.box[1:].strip()
+                url = tools.get_url_karusel() \
+                      + "/get?row=" + row_device \
+                      + "&column=" + column_device \
+                      + "&floor=" + floor_device
+                try:
+                    requests.get(url, timeout=3)
+                except requests.exceptions.ConnectTimeout:
+                    flash("На карусель запрос не ушёл :(")
