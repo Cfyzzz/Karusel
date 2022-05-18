676eddee23e0350e2607bbab17285f1fd28f6b4d
Alexander Nedovizin <parkit74@gmail.com>
2022-05-18 10:12:17 +0500

    upd (Отчет для закупа)

--- services/report_excel_file.py
+++ services/report_excel_file.py
@@ -1,6 +1,9 @@
 import os
 
+from openpyxl import Workbook
+
 import tools
+from model import Type, Component
 from .iservise import *
 
 ALLOWED_EXTENSIONS = {'xls', 'xlsx'}
@@ -21,7 +24,39 @@ class ReportToExcelFileGetService(IService):
         if os.access(path, os.F_OK):
             os.remove(path)
 
-        tools.report_to_excel(path)
+        self.report_to_excel(path)
         if os.access(path, os.F_OK):
             return send_file(path)
         return redirect("/")
+
+    @staticmethod
+    def report_to_excel(path):
+        """ Отчет о заканчивающихся деталях в Excel-файл
+
+        :param path: путь к файлу для выгрузки """
+        workbook = Workbook()
+        new_sheet = workbook.create_sheet("Отчёт для закупа")
+        new_sheet.append(["", "Наименование", "№ Ящика", "Ячейка", "Кол-во", "Описание", "Корпус", "Datasheet"])
+        for type_item in Type.select():
+            components = Component.select().where((Component.type == type_item) &
+                                                  (Component.quantity < Component.min_amount))
+            if components.count() == 0:
+                continue
+
+            new_sheet.append([type_item.type])
+            for component in components:
+                row = [
+                    "",
+                    component.designation,
+                    component.box,
+                    component.address,
+                    component.quantity,
+                    component.description,
+                    str(component.package) if component.package else "",
+                    component.datasheet
+                ]
+                new_sheet.append(row)
+        workbook.remove(workbook["Sheet"])
+        if not workbook.worksheets:
+            return
+        workbook.save(filename=path)
